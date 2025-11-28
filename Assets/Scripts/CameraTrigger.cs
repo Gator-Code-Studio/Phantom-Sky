ea19abf66419549648a2082951bc383028def7b4
Mo-Sanchez <moisessanchez6100@gmail.com>
2025-11-28 18:40:51 -0500

    Camera Fix Kinda

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -12,6 +12,9 @@ public class CameraTrigger : MonoBehaviour
     private bool hasMoved = false;
     private Vector3 targetPos;
 
+    private bool playerInside = false;
+    private bool enteredFromLeft = false;
+
     void Start()
     {
         mainCam = Camera.main;
@@ -21,23 +24,16 @@ public class CameraTrigger : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+
         if (isDirectional)
         {
             float playerX = other.transform.position.x;
             float triggerX = transform.position.x;
-            if (playerX < triggerX)
-            {
-                camHistory.Push(mainCam.transform.position);
-                targetPos = new Vector3(newCameraPosition.x, newCameraPosition.y, mainCam.transform.position.z);
-            }
-            else
-            {
-                Vector3 back = camHistory.Peek();
-                if (camHistory.Count > 1) camHistory.Pop();
-                targetPos = new Vector3(back.x, back.y, mainCam.transform.position.z);
-            }
 
-            moveCamera = true;
+            playerInside = true;
+
+            if (playerX < triggerX) enteredFromLeft = true;   // entered from left
+            else enteredFromLeft = false;                     // entered from right
         }
         else
         {
@@ -51,11 +47,45 @@ public class CameraTrigger : MonoBehaviour
         }
     }
 
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (!isDirectional) return;
+        if (!playerInside) return;
+
+        float playerX = other.transform.position.x;
+        float triggerX = transform.position.x;
+
+        // CASE 1: Entered from left, exited to the right -> move forward (push)
+        if (enteredFromLeft && playerX > triggerX)
+        {
+            camHistory.Push(mainCam.transform.position);
+            targetPos = new Vector3(newCameraPosition.x, newCameraPosition.y, mainCam.transform.position.z);
+            moveCamera = true;
+        }
+        // CASE 2 (VICE VERSA): Entered from right, exited to the left -> move back (pop)
+        else if (!enteredFromLeft && playerX < triggerX)
+        {
+            if (camHistory.Count > 0)
+            {
+                Vector3 back = camHistory.Peek();
+                if (camHistory.Count > 1) camHistory.Pop();
+                targetPos = new Vector3(back.x, back.y, mainCam.transform.position.z);
+                moveCamera = true;
+            }
+        }
+        // Any other combo (in left/out left, in right/out right) -> no move
+
+        playerInside = false;
+    }
+
     void Update()
     {
         if (!moveCamera) return;
+
         mainCam.transform.position =
             Vector3.MoveTowards(mainCam.transform.position, targetPos, moveSpeed * Time.deltaTime);
+
         if (Vector3.Distance(mainCam.transform.position, targetPos) < 0.05f) moveCamera = false;
     }
 }
